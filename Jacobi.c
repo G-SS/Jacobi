9a38376c140c7f536764f007681efc4eaf77bd62
G-SS <53111942+G-SS@users.noreply.github.com>
2020-10-13 16:37:40 +0800

    Update Jacobi.c

--- Jacobi.c
+++ Jacobi.c
@@ -1,14 +1,25 @@
+/**
+ * @brief Jacobi 雅可比迭代法
+ * @param[in]  a 系数矩阵
+ * @param[in]  row 矩阵行数
+ * @param[in]  col 矩阵列数
+ * @param[in]  e 精度
+ * @param[in]  b
+ * @param[out] y 解
+ */
 void Jacobi(double **a, int row, int col, double e, double *b, double *y)
 {
+    /// 初始向量
     double *x = (double *)malloc(sizeof(double) * row);
     memset(x, 0, sizeof(sizeof(double) * row));
 
     double sum = 0;
-    int cnt = 0;
+    int cnt = 0;        // 迭代计数   
     int i = 0;
 
     while(1)
     {
+        /// 迭代
         for(i=0; i<row; ++i)
         {
             for(int j=0; j<col; ++j)
@@ -21,13 +32,15 @@ void Jacobi(double **a, int row, int col, double e, double *b, double *y)
 
         i = 0;
 
+        /// 精度判断
         while(i<row)
         {
             if(fabs(x[i] - y[i]) > e)
                 break;
             ++i;
         }
-
+      
+        /// 更新初始向量
         if(i != row)
         {
             for(i=0; i<row; ++i)
